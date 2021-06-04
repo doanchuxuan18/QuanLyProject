dfff616d214c2e6fba323df7359c56f40023dffa
doanchuxuan18 <84855872+doanchuxuan18@users.noreply.github.com>
2021-06-04 20:15:26 +0700

    first commit

---
+++ QuanLyProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuanLyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
